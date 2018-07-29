5191f331422923710e8616af414e180767e2a931
heavyrain.lee <heavyrain.lee@gmail.com>
2018-07-29 09:16:14 +0800

    modify frontend ui

--- shortlink/server.go
+++ shortlink/server.go
@@ -47,6 +47,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 		if entry.Password != "" {
 			password := r.URL.Query().Get("pwd")
+			if password == "" {
+				fmt.Fprintf(w, "<!DOCTYPE html>" +
+				"<html> "+
+				"<head><title>请输入密码</title><meta http-equiv='Content-Type' content='text/html; charset=utf-8'></head> "+
+				"<body><h1>请输入访问密码</h1>密码：<input type='text' id='pwd' /></br></br><button type='button' onclick='myFunction()'>Go</button><script> function myFunction(){var elementPwd = document.getElementById('pwd'); var url = self.location.href;if (elementPwd.value != '') {window.location.href = url + '?pwd=' + elementPwd.value;}}</script></body> "+
+				"</html>")
+				return
+			}
 			if password != entry.Password {
 				w.WriteHeader(http.StatusNotFound)
 				fmt.Fprintf(w, "密码错误")
@@ -113,7 +121,7 @@ func createHandler(w http.ResponseWriter, r *http.Request) {
 	"<head> <title>短链生成器</title> <meta http-equiv='Content-Type' content='text/html; charset=utf-8'> <script src='http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.0.js'> </script> </head> " +
 	"<body> <h1>短链生成器</h1> <p id='demo'>输入Url，点击确认，生成短链</p> URL：<input type='text' id='url' /> 有效期(秒)：<input type='text' size = '6' id='duration' /> 密码：<input type='text' size = '10' id='pwd' /> </br></br> <button type='button' onclick='myFunction()'>确认</button></br></br> 结果: <a href='' id='resulta'></a>" +
 		"</br></br><a href='https://github.com/tomlee0201/shortl'>源码</a>" +
-	"<script> function myFunction(){ var resulta = document.getElementById('resulta'); var elementUrl = document.getElementById('url'); var elementDuration = document.getElementById('duration'); var elementPwd = document.getElementById('pwd'); resulta.innerHTML = ''; resulta.setAttribute('href', '');   $.post('/api/create',{url:elementUrl.value,duration:elementDuration.value,password:elementPwd.value}, function(data,status){if(status = 'success') { var shortUrl = 'http://' + data.domain; if(data.port != '80') { shortUrl = shortUrl + ':' + data.port; } shortUrl = shortUrl + '/' + data.key; if(elementPwd.value !='') {shortUrl = shortUrl + '?pwd=' + elementPwd.value}  resulta.innerHTML = shortUrl;resulta.setAttribute('href', shortUrl); if(elementDuration.value != ''){alert('请在 ' + elementDuration.value + 's 内访问')}} else {alert('错误：' + data);}}); }</script>" +
+	"<script> function myFunction(){ var resulta = document.getElementById('resulta'); var elementUrl = document.getElementById('url'); var elementDuration = document.getElementById('duration'); var elementPwd = document.getElementById('pwd'); resulta.innerHTML = ''; resulta.setAttribute('href', '');   $.post('/api/create',{url:elementUrl.value,duration:elementDuration.value,password:elementPwd.value}, function(data,status){if(status = 'success') { var shortUrl = 'http://' + data.domain; if(data.port != '80') { shortUrl = shortUrl + ':' + data.port; } shortUrl = shortUrl + '/' + data.key; resulta.innerHTML = shortUrl;resulta.setAttribute('href', shortUrl); if(elementDuration.value != ''){alert('请在 ' + elementDuration.value + 's 内访问')}} else {alert('错误：' + data);}}); }</script>" +
 	"</body> </html> ")
 }
 
